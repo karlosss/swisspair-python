accd951e4644729a9f295ab67fcd6efa2b869022
Karel Jilek <los.karlosss@gmail.com>
2024-10-09 02:10:25 +0200

    improve interface

--- src/pyswisspair/interface.py
+++ src/pyswisspair/interface.py
@@ -1,27 +1,27 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 import swisspair
 
 @dataclass
 class Player:
     id: str
-    previous_opponents_ids: set[str]
-    had_bye: bool
     points: int
     rank: int
+    can_get_bye: bool = True
+    cannot_be_paired_against_ids: set[str] = field(default_factory=set)
 
     def compile(self) -> swisspair.Player:
         p = swisspair.Player()
         p.id = self.id
-        p.previous_opponents_ids = self.previous_opponents_ids
-        p.had_bye = self.had_bye
+        p.cannot_be_paired_against_ids = self.cannot_be_paired_against_ids
+        p.can_get_bye = self.can_get_bye
         p.points = self.points
         p.rank = self.rank
         return p
 
     @staticmethod
     def decompile(player: swisspair.Player) -> "Player":
-        return Player(id=player.id, previous_opponents_ids=player.previous_opponents_ids, had_bye=player.had_bye, points=player.points, rank=player.rank)
+        return Player(id=player.id, cannot_be_paired_against_ids=player.cannot_be_paired_against_ids, can_get_bye=player.can_get_bye, points=player.points, rank=player.rank)
 
 @dataclass
 class Match:
@@ -36,6 +36,23 @@ class Match:
     def is_bye(self) -> bool:
         return self.p2 is None
 
-def create_matches(players: list[Player], power_pairing: bool) -> list[Match]:
+
+def _validate_players(players: list[Player]) -> None:
+    id_to_player = {p.id: p for p in players}
+    assert len(id_to_player) == len(players), "Player IDs must be unique"
+
+    for i in range(len(players)):
+        assert players[i].rank == i+1, "Players must be ranked 1, 2, 3, ..."
+
+        if i > 0:
+            assert players[i].points <= players[i-1].points, "Points must not increase as the ranks decrease"
+
+        for cannot_id in players[i].cannot_be_paired_against_ids:
+            assert cannot_id in id_to_player, "Nonexistent player ID in cannot-be-paired-against set"
+            assert players[i].id in id_to_player[cannot_id].cannot_be_paired_against_ids, "cannot-be-paired-against must be symmetric"
+
+
+def create_matches(players: list[Player], power_pairing: bool = False) -> list[Match]:
+    _validate_players(players)
     matches = swisspair.create_matches([p.compile() for p in players], power_pairing)
     return [Match.decompile(m) for m in matches]
--- test.py
+++ test.py
@@ -5,23 +5,24 @@ from src.pyswisspair import Player, create_matches
 
 players = []
 
-for i in range(100):
+n = 9999
+
+for i in range(n):
     players.append(
         Player(
             id="P" + str(i),
-            previous_opponents_ids=set(),
-            had_bye=False,
-            points=i,
+            can_get_bye=True,
+            points=n-i // 20,
             rank=i+1,
         )
     )
 
 import time
 start = time.time()
-matches = create_matches(players, True)
+matches = create_matches(players, False)
 duration = time.time()-start
-
-print(duration)
 
 for table_number, match in enumerate(matches):
     print(table_number+1, match)
+
+print(duration)
